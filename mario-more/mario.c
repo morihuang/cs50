c31d211eebba9585ec282fc2bb1b2bc54f1be2c8
Mori <moriya201412@gmail.com>
2022-05-02 13:31:11 +0000

    Mon, May 2, 2022, 1:31 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,10 +3,14 @@
 
 int main(void)
 {
-    int ans_layer = get_int("Height: \n");
-    if(ans_layer > 1 || ans_layer < 8):
+    do
     {
-        for(i=0, i < ans_layer, i++):
+        int ans_layer = get_int("Height: \n");
+    }
+    while (ans_layer < 1 || ans_layer > 8);
+
+
+    for(i=0, i < ans_layer, i++)
         {
             for (j=0, j < ans_layer, j++):
             {
