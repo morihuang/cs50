13ace1719bb54bc8d54464af6786c62005082bf3
Mori <moriya201412@gmail.com>
2022-05-02 13:22:01 +0000

    Mon, May 2, 2022, 1:22 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int ans_layer = get_int("Set layer level of a pyramid: \n");
+    for(i=0, i < ans_layer, i++):
+    {
+        for (j=0, j < ans_layer, j++):
+        {
+            printf("#")
+            printf("  ")
+            printf("#")
+        }
+    }
 }
