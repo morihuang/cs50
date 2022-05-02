a9797520f61a9b1b49a04ebe02551469ded495f0
Mori <moriya201412@gmail.com>
2022-05-02 14:37:58 +0000

    Mon, May 2, 2022, 2:37 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -12,18 +12,18 @@ int main(void)
 
 
     int i, j, k, l, m;
-    while (i=0, i < ans_layer, i++)
+    while (i=0, i < ans_layer, i+=1)
     {
-        while (j=0, j < ans_layer, j++)
+        while (j=0, j < ans_layer, j+=1)
         {
             //spaces
-            while (k=0, k < ans_layer-1, k++)
+            while (k=0, k < ans_layer-1, k+=1)
             {
                 printf(" ");
             }
 
             //#
-            while (l=0, l < i, l++)
+            while (l=0, l < i, l+=1)
             {
                 printf("#");
             }
@@ -32,7 +32,7 @@ int main(void)
             printf("  ");
 
             //#
-            while (m=0, m < i, m++)
+            while (m=0, m < i, m+=1)
             {
                 printf("#\n");
             }
