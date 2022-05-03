16b54951b69c3de65ecb8433cb5a7c7bd5f25380
Mori <moriya201412@gmail.com>
2022-05-03 09:25:07 +0000

    Tue, May 3, 2022, 9:25 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -41,17 +41,17 @@ int main(void)
     long x_ans = ans;
 
 
-    for (j = 1; j < i; j += 2)
+    for (j = 1; j <= i; j += 2)
     {
         //even digit * 2
-        t1_ans = (x_ans / (int) pow(10, j)) % 10;
+        t1_ans = (x_ans / (long) pow(10, j)) % 10;
         //printf("%li, %li\n", x_ans, t_ans);
 
         //odd digit
-        t2_ans = (x_ans / (int) pow(10, j-1)) % 10;
+        t2_ans = (x_ans / (long) pow(10, j-1)) % 10;
 
         t3_ans += t1_ans * 2 + t2_ans;
-        printf("%li, %li, %li\n", t1_ans, t2_ans, t3_ans);
+        printf("%li, %li, %li, %i\n", t1_ans, t2_ans, t3_ans, j);
     }
 
     if (t3_ans % 10 == 0)
