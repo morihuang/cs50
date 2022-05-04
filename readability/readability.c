878d15ca5e0de8d5a1fd2748a8d352dd4eccd81c
Mori <moriya201412@gmail.com>
2022-05-04 07:39:47 +0000

    Wed, May 4, 2022, 7:39 AM +00:00

---
+++ readability/readability.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //get string
+    string ans = get_string("Text: ")
+
+    //count letter, words, sentences,
+
+    //print output
+    printf("Grade %", )
+    
+}
