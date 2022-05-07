c8bc9dc18b49518fb239d9fdce2f0240cd91f1c2
Mori <moriya201412@gmail.com>
2022-05-07 11:03:07 +0000

    Sat, May 7, 2022, 11:03 AM +00:00

---
+++ script/script.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int a = get_int("The length of 1st side: \n");
+    int b = get_int("The length of 2nd side: \n");
+    int c = get_int("The length of 3rd side: \n");
+
+
+}
+
+int valid_triangle(a, b, c)
+{
+    int a, b, c;
+    if a + b < c
+}
