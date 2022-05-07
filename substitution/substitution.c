86f2ef95332bc941859ca1a140bbfae533f4e0ed
Mori <moriya201412@gmail.com>
2022-05-07 15:08:14 +0000

    Sat, May 7, 2022, 3:08 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -9,18 +9,7 @@ int main(int argc, string argv[])
     string s = argv[1];
     int len = strlen(s);
 
-    //printf("%s, %i\n", s, len);
-    //printf("%s, %i, %i\n", s, len, ab);
-    /*for (int i = 0; i < len; i++)
-    {
-        if (isalpha(s[i]))
-        {
-        printf("yes!\n");
-        }
-    }*/
-
     //confirm the number of the string
-
     if (len != 26)
     {
         printf("Key must contain 26 characters.\n");
@@ -30,13 +19,11 @@ int main(int argc, string argv[])
     //confirm all alphbets
     for (int i = 0; i < len; i++)
     {
-        //printf("%c\n", s[i]);
         if (isalpha(s[i]) == 0)
         {
             printf("Key must only contain alphabetic characters.\n");
             return 1;
         }
-
     }
 
     //confirm enter correct
@@ -45,7 +32,6 @@ int main(int argc, string argv[])
         printf("Usage: ./substitution key\n");
         return 1;
     }
-
 
     //confirm non repeat
     for (int i = 0; i < len; i++)
@@ -58,7 +44,6 @@ int main(int argc, string argv[])
                 printf("%i, %i\n", i, j);
                 printf("Key must not contain repeated characters.\n");
                 return 1;
-                //break;
             }
         }
     }
