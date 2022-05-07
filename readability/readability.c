9ceb6f4c26b983027b6dfa47088f1e3f93456865
Mori <moriya201412@gmail.com>
2022-05-07 02:46:36 +0000

    Sat, May 7, 2022, 2:46 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+
+int count_letters(string text);
 
 int main(void)
 {
     string ans = get_string ("Text: ");
     printf("%s\n", ans);
+}
+
+int count_letters(string text)
+{
+
 }
