862c4b05e27437521b7e91255f4919f7ae1d1cc0
Mori <moriya201412@gmail.com>
2022-05-07 11:09:12 +0000

    Sat, May 7, 2022, 11:09 AM +00:00

--- script/script.c
+++ script/script.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 #include <cs50.h>
 
+bool valid_triangle(float a, float b, float c);
+
 int main(void)
 {
     int a = get_int("The length of 1st side: \n");
     int b = get_int("The length of 2nd side: \n");
     int c = get_int("The length of 3rd side: \n");
 
+    int x = valid_triangle(a, b, c);
+    if (x != 0)
+    {
+        printf("valid!\n");
+    }
+    else
+    {
+        printf("not valid!\n");
+    }
+
 
 }
 
-int valid_triangle(a, b, c)
+bool valid_triangle(float a, float b, float c)
 {
-    int a, b, c;
-    if a + b < c
+    if (a < 0 || b < 0 || c < 0)
+    {
+        return false;
+    }
+    if (a + b <= c || a + c <= b || b + c <= a)
+    {
+        return false;
+    }
+    return true;
 }
