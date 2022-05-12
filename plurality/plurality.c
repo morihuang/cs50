45808377f3efe566812bcc6e4492765ab9acbae4
Mori <moriya201412@gmail.com>
2022-05-12 02:45:37 +0000

    Thu, May 12, 2022, 2:45 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -74,13 +74,8 @@ bool vote(string name)
             candidates[i].votes++;
             return true;
         }
-
-        else
-        {
-            return false;
-        }
     }
-    return;
+    return false;
 }
 
 // Print the winner (or winners) of the election
