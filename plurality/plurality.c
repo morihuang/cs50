44cd9c1949f5c26bf2d4054e9159f3a27628298e
Mori <moriya201412@gmail.com>
2022-05-12 03:26:36 +0000

    Thu, May 12, 2022, 3:26 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -69,10 +69,9 @@ bool vote(string name)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if (name == candidates[i].name)
+        if (strcmp(name, candidates[i].name) == 0)
         {
             candidates[i].votes++;
-            printf("yeh");
             return true;
             break;
         }
