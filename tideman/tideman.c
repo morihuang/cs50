01e0ae0163ab5dd903041f180062b19d703d4dc3
Mori <moriya201412@gmail.com>
2022-05-14 08:34:57 +0000

    Sat, May 14, 2022, 8:34 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -102,7 +102,7 @@ bool vote(int rank, string name, int ranks[])
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidates[i].name) == 0)
+        if (strcmp(name, candidates[i]) == 0)
         {
             ranks[rank] = name;
             return true;
