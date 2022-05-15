05d62241982ded9eb0a5369c1ca2cc59f451c199
Mori <moriya201412@gmail.com>
2022-05-15 13:34:32 +0000

    Sun, May 15, 2022, 1:34 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -132,7 +132,7 @@ void add_pairs(void)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = i + 1; j < candidate_count; j++)
         {
             if (preferences[i][j] > preferences[j][i])
             {
