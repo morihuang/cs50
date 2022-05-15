f1cfa0892ef956f9cea826242ad157e18ee3a825
Mori <moriya201412@gmail.com>
2022-05-15 15:14:34 +0000

    Sun, May 15, 2022, 3:14 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -188,9 +188,9 @@ void sort_pairs(void)
         swap_counter = 0;*/
         for (int i = 0; i < pair_count; i++)
         {
-            for (int j = i + 1; j < pair_count; j++)
+            for (int j = 0; j < pair_count - 1; j++)
             {
-                if (preferences[pairs[j].winner][pairs[j].loser] > preferences[pairs[j + 1].winner][pairs[j + 1].loser])
+                if (preferences[pairs[j].winner][pairs[j].loser] < preferences[pairs[j + 1].winner][pairs[j + 1].loser])
                 {
                     pair temp = pairs[j];
                     pairs[j] = pairs[j + 1];
