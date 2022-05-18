b56c46f4da1adf391e638e095657d582be27e271
Mori <moriya201412@gmail.com>
2022-05-18 13:43:40 +0000

    Wed, May 18, 2022, 1:43 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -218,20 +218,23 @@ void print_winner(void)
     while (!winner)
     {
         for (int i = 0; i < candidate_count; i++)
+        {
+            winner = true;
             for (int j = 0; j < candidate_count; j++)
             {
                 if (locked[j][i] == true)
                 {
                     winner = false;
-
+                    break;
                 }
             }
 
-
-    }
-    if (winner == true)
-    {
-        printf("%s", candidates[i]);
+            if (winner == true)
+            {
+                printf("%s", candidates[i]);
+                break;
+            }
+        }
     }
     return;
 }
