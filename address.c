93c97583c08fef97467bccd9e93f0f15b6e6387d
Mori <moriya201412@gmail.com>
2022-05-30 12:47:13 +0000

    Mon, May 30, 2022, 12:47 PM +00:00

--- address.c
+++ address.c
@@ -32,5 +32,11 @@ int main(void)
 
     printf("%p\n", p);
     printf("%p\n", s);
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
+    printf("%p\n", &s[4]);
+    printf("%p\n", &s[5]);
 
 }
