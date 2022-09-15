64c357f90eef3198e7b0a11dd21849fd3eeb0c07
Mori <moriya201412@gmail.com>
2022-09-15 16:16:32 +0000

    Thu, Sep 15, 2022, 4:16 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -123,6 +123,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         counter++;
                     }
                 }
+                break;
             }
 
             //left upper corner
@@ -138,6 +139,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         counter++;
                     }
                 }
+                break;
             }
 
             //right upper corner
@@ -153,6 +155,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         counter++;
                     }
                 }
+                break;
             }
 
             //left lower corner
@@ -168,6 +171,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         counter++;
                     }
                 }
+                break;
             }
 
             //right lower corner
@@ -183,6 +187,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         counter++;
                     }
                 }
+                break;
             }
 
             //upper edge
@@ -198,6 +203,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         counter++;
                     }
                 }
+                break;
             }
 
             //lower edge
@@ -213,6 +219,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         counter++;
                     }
                 }
+                break;
             }
 
             //left edge
@@ -228,6 +235,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         counter++;
                     }
                 }
+                break;
             }
 
             //right edge
@@ -243,6 +251,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         counter++;
                     }
                 }
+                break;
             }
 
             image[i][j].rgbtRed = round(tmpRed / counter);
