e833e7abaa645a4ec941870483406c92d0c3c1be
Mori <moriya201412@gmail.com>
2022-09-15 09:20:44 +0000

    Thu, Sep 15, 2022, 9:20 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int tmp = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3);
+            int tmp = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
             image[i][j].rgbtRed = tmp;
             image[i][j].rgbtGreen = tmp;
             image[i][j].rgbtBlue = tmp;
