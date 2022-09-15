49b7b5d1135a3991dd3fb5dcbfbcdee82bbdd362
Mori <moriya201412@gmail.com>
2022-09-15 09:49:37 +0000

    Thu, Sep 15, 2022, 9:49 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -27,8 +27,23 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             float sepiaRed =
-                .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
-            
+                round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
+            float sepiaGreen =
+                round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+            float sepiaBlue =
+                round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
             //return;
         }
     }
