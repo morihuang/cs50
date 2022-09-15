0b7637947340bc49382efae4d61adfdece1888c7
Mori <moriya201412@gmail.com>
2022-09-15 09:52:35 +0000

    Thu, Sep 15, 2022, 9:52 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -66,6 +66,18 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float tmp =
+                round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            image[i][j].rgbtRed = tmp;
+            image[i][j].rgbtGreen = tmp;
+            image[i][j].rgbtBlue = tmp;
+            //return;
+        }
+    }
     return;
 }
 
