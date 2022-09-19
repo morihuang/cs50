847e3a5d0331da64789e62900779c5de33b64df2
Mori <moriya201412@gmail.com>
2022-09-19 13:08:47 +0000

    Mon, Sep 19, 2022, 1:08 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -81,11 +81,17 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
+    if (p == NULL)
+    {
+        return;
+    }
 
     // TODO: Free parents recursively
+    free_family(p->parent[0]);
+    free_family(p->parent[1]);
 
     // TODO: Free child
-
+    
 }
 
 // Print each family member and their alleles.
