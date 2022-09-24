074652a03710409c1ce487785c6d36525d204a8f
Mori <moriya201412@gmail.com>
2022-09-24 14:21:50 +0000

    Sat, Sep 24, 2022, 2:21 PM +00:00

--- speller/dictionary.h
+++ speller/dictionary.h
@@ -10,9 +10,9 @@
 #define LENGTH 45
 
 // Prototypes
-bool check(const char *word);
-unsigned int hash(const char *word);
-bool load(const char *dictionary);
+bool check(const char *word); //bool check(const string word)
+unsigned int hash(const char *word); //unsigned int hash(const string word)
+bool load(const char *dictionary); //bool load(const string dictionary)
 unsigned int size(void);
 bool unload(void);
 
