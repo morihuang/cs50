0bf88c370a2f20830cfd58a9d3e1e3e244254365
Mori <moriya201412@gmail.com>
2022-10-06 08:49:35 +0000

    Thu, Oct 6, 2022, 8:49 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -51,7 +51,7 @@ bool load(const char *dictionary)
     }
 
     // read words into an array
-    char tmp_word;
+    char tmp_word[0];
     int check_end = 0;
     bool ans = false;
     while (check_end != EOF)
